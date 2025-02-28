96616992c40da7f17c81e890d085494521f63beb
Michal Töpfer <michal.topfer@gmail.com>
2025-02-28 15:31:34 +0100

    Allow setting reasoning effort for o3-mini

--- adaptations/openai.py
+++ adaptations/openai.py
@@ -8,9 +8,14 @@ class OpenAIAdaptation(LLMAdaptation):
     @staticmethod
     def create_llm(model="gpt-4o-mini-2024-07-18", config=None):
         print("LLM model:", model)
-        if config is not None \
-                and "adaptation_params" in config \
-                and "temperature" in config["adaptation_params"]:
-            return ChatOpenAI(model=model, max_tokens=None, temperature=config["adaptation_params"]["temperature"])
-        else:
-            return ChatOpenAI(model=model, max_tokens=None)
+
+        kwargs = {}
+
+        temperature = config.get("adaptation_params", {}).get("temperature", None)
+        if temperature is not None:
+            kwargs["temperature"] = temperature
+        reasoning_effort = config.get("adaptation_params", {}).get("reasoning_effort", None)
+        if reasoning_effort is not None:
+            kwargs["reasoning_effort"] = reasoning_effort
+
+        return ChatOpenAI(model=model, max_tokens=None, **kwargs)
