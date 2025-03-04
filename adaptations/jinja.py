81f98c1e0e282c09d09f14868e15e70e75f319a4
Michal Töpfer <michal.topfer@gmail.com>
2025-03-04 10:15:22 +0100

    Implement ensemble-first answer format

--- adaptations/jinja.py
+++ adaptations/jinja.py
@@ -47,17 +47,22 @@ def get_attr(component, attribute, config):
 
 
 @pass_context
-def get_ensembles(context, config: list[str | dict]):
+def get_ensembles_ctx(context, config: list[str | dict]):
     components = context.get("components")
     ensemble_types = context.get("configuration")["ensembles"]
+    environment = context.get("environment")
 
+    yield from get_ensembles(components, config, ensemble_types, environment)
+
+
+def get_ensembles(components, config, ensemble_types, environment):
     for ensemble in config:
         if isinstance(ensemble, str):  # singleton
             yield ensemble_types[ensemble]
         else:  # instance for each component
             ensemble_type = ensemble_types[ensemble["type"]]
-            component_type = eval(ensemble["foreach"], context.get("environment").get_globals())
-            condition = eval(ensemble.get("if", "True"), context.get("environment").get_globals())
+            component_type = eval(ensemble["foreach"], environment.get_globals())
+            condition = eval(ensemble.get("if", "True"), environment.get_globals())
 
             components = filter(lambda c: isinstance(c, component_type), components)
             components = filter(condition, components)
@@ -81,7 +86,7 @@ def prepare_jinja_env():
     jinja_env.filters['get_components'] = get_components_ctx
     jinja_env.filters['show_attr'] = show_attr
     jinja_env.filters['get_attr'] = get_attr
-    jinja_env.filters['get_ensembles'] = get_ensembles
+    jinja_env.filters['get_ensembles'] = get_ensembles_ctx
     return jinja_env
 
 
@@ -112,8 +117,11 @@ class JinjaPromptTemplate(PromptTemplate):
 
         components = self.load_components_for_assignments(simulation)
 
-        # TODO: this is only component-first, add also ensemble-first
-        errors = self.process_component_first(answer, components, simulation)
+        # TODO: this is defined by the first assignment, we need to change the DSL to have global answer format
+        if next(iter(self.configuration["assignments"].values()))["answer_format"] == "component-first":
+            errors = self.process_component_first(answer, components, simulation)
+        else:
+            errors = self.process_ensemble_first(answer, components, simulation)
 
         return errors, memory
 
@@ -152,18 +160,80 @@ class JinjaPromptTemplate(PromptTemplate):
                     errors.append(ProcessingError(row, error))
             except (ValueError, KeyError, IndexError) as error:  # if error is not caught inside assign_group, we don't retry
                 print(f"Error - invalid row ({repr(error)}): {repr(row)}")
+        if len(simulation.assignments) < len(components):
+            missing_components = [component_id for component_id, component in components.items() if component not in simulation.assignments]
+            error = "The following components have not been assigned to a group: " + ", ".join(missing_components)
+            errors.append(ProcessingError(None, error))
+            simulation.append_assignment_error(error)
+        return errors
+
+    def process_ensemble_first(self, answer, components, simulation):
+        rows = answer.split("\n")
+        errors: list[ProcessingError] = []
+        assigned_groups = set()
+
+        for row in rows:
+            try:
+                if row == "" or row == "```":
+                    continue
+                row = row.replace("- ", "")  # remove leading hyphens
+                row = row.replace("**", "")  # remove bold
+
+                group, components_list = row.split(":")
+                group = group.strip()
+                assigned_groups.add(group)
+                component_ids = components_list.strip().split(",")
+
+                for component_id in component_ids:
+                    component_id = component_id.strip()
+
+                    if component_id == "":
+                        continue
+                    if component_id not in components:
+                        error = f"Unknown component: {component_id}"
+                        errors.append(ProcessingError(row, error))
+                        simulation.append_assignment_error(error)
+                        continue
+
+                    component = components[component_id]
+                    error = simulation.assign_group(component, group.strip())
+                    if error:
+                        errors.append(ProcessingError(row, error))
+            except (ValueError, KeyError, IndexError) as error:  # if error is not caught inside assign_group, we don't retry
+                print(f"Error - invalid row ({repr(error)}): {repr(row)}")
+        if len(simulation.assignments) < len(components):
+            missing_components = [component_id for component_id, component in components.items() if component not in simulation.assignments]
+            error = "The following components have not been assigned to a group: " + ", ".join(missing_components)
+            errors.append(ProcessingError(None, error))
+            simulation.append_assignment_error(error)
+
+        all_groups = self.load_ensembles_for_assignments(simulation)
+        if len(assigned_groups) < len(all_groups):
+            missing_groups = [group for group in all_groups if group not in assigned_groups]
+            error = "The following groups are missing in the assignment: " + ", ".join(missing_groups)
+            errors.append(ProcessingError(None, error))
+            simulation.append_assignment_error(error)
         return errors
 
     def load_components_for_assignments(self, simulation):
         components = {}
 
         for assignment_config in self.configuration["assignments"].values():
-            component_type = assignment_config["components"]["type"]
-            component_config = self.configuration["components"][component_type]
-            id_attr = component_config.get("id", "id")
+            component_config = assignment_config["components"]
+            component_type_config = self.configuration["components"][component_config["type"]]
+            id_attr = component_type_config.get("id", "id")
 
             for component in get_components(component_config, simulation.components, simulation):
                 component_id = getattr(component, id_attr)
                 components[component_id] = component
 
         return components
+
+    def load_ensembles_for_assignments(self, simulation):
+        ensembles = set()
+
+        for assignment_config in self.configuration["assignments"].values():
+            assignment_ensembles = get_ensembles(simulation.components, assignment_config["ensembles"], self.configuration["ensembles"], simulation)
+            ensembles.update([ensemble["name"] for ensemble in assignment_ensembles])
+
+        return ensembles
